998886239fea27b2a51e5cf8390bc6c00d52d8b6
AnastasiyaKuznetsova <anastasia.kuznezova@yandex.ru>
2023-05-03 17:42:41 +0300

    new project

--- bot/models.py
+++ bot/models.py
@@ -3,11 +3,5 @@ import sqlite3
 
 # Create your models here.
 
-class BotDB:
 
-    def __init__(self, db_file):
-        """Инициализация соединения с БД"""
-        self.connect = sqlite3.connect(db_file)
-        self.cursor = self.connect.cursor()
 
-    
